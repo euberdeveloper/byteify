21bbe66da6377e2bb7c9aaf522117389a230e767
nicolatoscan <nicolatoscan99@outlook.it>
2021-04-01 05:04:44 +0200

    test: basic test cases

--- test/serializeTestCases.copy.ts
+++ test/serializeTestCases.copy.ts
@@ -8,6 +8,7 @@ interface ITestCase<T> {
 }
 
 const testCases = [
+    // ----- Bool
     {
         name: 'Bool',
         serializeFn: byteify.serializeBool,
@@ -17,6 +18,7 @@ const testCases = [
             [false, [0]]
         ]
     } as ITestCase<boolean>,
+    // ----- Uint
     {
         name: 'Uint8',
         serializeFn: byteify.serializeUint8,
@@ -34,7 +36,82 @@ const testCases = [
             [1, [0, 1]],
             [0, [0, 0]]
         ]
+    } as ITestCase<number>,
+    {
+        name: 'Uint32',
+        serializeFn: byteify.serializeUint32,
+        deserializeFn: byteify.deserializeUint32,
+        values: [
+            [1, [0, 0, 0, 1]],
+            [0, [0, 0, 0, 0]]
+        ]
+    } as ITestCase<number>,
+    {
+        name: 'Uint64',
+        serializeFn: byteify.serializeUint64,
+        deserializeFn: byteify.deserializeUint64,
+        values: [
+            [1, [0, 0, 0, 0, 0, 0, 0, 1]],
+            [0, [0, 0, 0, 0, 0, 0, 0, 0]]
+        ]
+    } as ITestCase<number>,
+    // ----- Int
+    {
+        name: 'Int8',
+        serializeFn: byteify.serializeInt8,
+        deserializeFn: byteify.deserializeInt8,
+        values: [
+            [1, [1]],
+            [0, [0]]
+        ]
+    } as ITestCase<number>,
+    {
+        name: 'Int16',
+        serializeFn: byteify.serializeInt16,
+        deserializeFn: byteify.deserializeInt16,
+        values: [
+            [1, [0, 1]],
+            [0, [0, 0]]
+        ]
+    } as ITestCase<number>,
+    {
+        name: 'Int32',
+        serializeFn: byteify.serializeInt32,
+        deserializeFn: byteify.deserializeInt32,
+        values: [
+            [1, [0, 0, 0, 1]],
+            [0, [0, 0, 0, 0]]
+        ]
+    } as ITestCase<number>,
+    {
+        name: 'Int64',
+        serializeFn: byteify.serializeInt64,
+        deserializeFn: byteify.deserializeInt64,
+        values: [
+            [1, [0, 0, 0, 0, 0, 0, 0, 1]],
+            [0, [0, 0, 0, 0, 0, 0, 0, 0]]
+        ]
+    } as ITestCase<number>,
+    // ----- Float
+    {
+        name: 'Float32',
+        serializeFn: byteify.serializeFloat32,
+        deserializeFn: byteify.deserializeFloat32,
+        values: [
+            [1, [0, 0, 128, 63]],
+            [0, [0, 0, 0, 0]]
+        ]
+    } as ITestCase<number>,
+    {
+        name: 'Float64',
+        serializeFn: byteify.serializeFloat64,
+        deserializeFn: byteify.deserializeFloat64,
+        values: [
+            [1, [0, 0, 0, 0, 0, 0, 240, 63]],
+            [0, [0, 0, 0, 0, 0, 0, 0, 0]]
+        ]
     } as ITestCase<number>
+
 ];
 
 export default testCases;
