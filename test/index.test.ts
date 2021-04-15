3cdd9ba54e4bb16a071d4d11ee81c5326f74e974
euberdeveloper <euberdeveloper@gmail.com>
2021-04-15 23:29:21 +0200

    style: update euber eslint
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 import generalCases from './general-cases/general-cases.test';
 import erroredCases from './errored-cases/errored-cases.test';
 
-describe('byteify tests', () => {
+describe('byteify tests', function () {
     generalCases();
     erroredCases();
 });
