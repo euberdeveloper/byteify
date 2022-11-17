ff76347548b539c01cef52b901fdbfa33bb79567
euberdeveloper <euberdeveloper@gmail.com>
2022-11-17 14:52:22 +0100

    fix(source): export only exported types
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,5 @@
 export * as limits from './values/limits';
-export * from './types';
+export * from './types/exported';
 export * from './modules/deserialize';
 export * from './modules/serialize';
 export * from './errors';
