e63b98d771835d26941a0ba7b0f08a8661a52432
euberdeveloper <euberdeveloper@gmail.com>
2023-02-10 23:03:54 +0100

    fix(source): fix exports with esm
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,5 @@
-export * as limits from './values/limits';
-export * from './types/exported';
-export * from './modules/deserialize';
-export * from './modules/serialize';
-export * from './errors';
+export * as limits from './values/limits.js';
+export * from './types/exported.js';
+export * from './modules/deserialize.js';
+export * from './modules/serialize.js';
+export * from './errors/index.js';
