301f2c1669810a71e14424b2ad4c593669685bfa
euberdeveloper <euberdeveloper@gmail.com>
2023-02-10 23:10:44 +0100

    test(jest.config.ts): adjust for esm
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,31 @@
 import type { Config } from '@jest/types';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import tsconfigJson from './tsconfig.json';
+
+function manageKey(key: string): string {
+   return key.includes('(.*)') ? key.slice(0, -1) + '\\.js$' : key;
+}
+function manageMapper(mapper: Record<string, string>): Record<string, string> {
+   const newMapper: Record<string, string> = {};
+   for (const key in mapper) {
+      newMapper[manageKey(key)] = mapper[key];
+   }
+   newMapper['^(.*).js$'] = '$1';
+   return newMapper;
+}
 
 const config: Config.InitialOptions = {
     preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'node',
     verbose: true,
-    transform: {
-        '^.+\\.tsx?$': ['ts-jest', {
+    globals: {
+        'ts-jest': {
             tsconfig: './tsconfig.json',
-        }]
-    },    
-    collectCoverageFrom: ["./source/**/*.ts"],
+            useESM: true
+        }
+    },
     coverageProvider: 'v8',
+    moduleNameMapper: manageMapper(pathsToModuleNameMapper(tsconfigJson.compilerOptions.paths, { prefix: '<rootDir>/' }) as Record<string, string>),
     transformIgnorePatterns: ['<rootDir>/node_modules/']
 };
 export default config;
